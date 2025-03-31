f3d23bc96392fba61e4330b11730bfca5d3cbeaf
Hans Meyer <hans.meyer@team-beverage.de>
2025-03-31 14:59:35 +0200

    Frontend init

--- src/aspire/Demo.AppHost/Program.cs
+++ src/aspire/Demo.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.DemoServer>("demoserver");
 
+builder.AddProject<Projects.frontend>("frontend");
+
 builder.Build().Run();
